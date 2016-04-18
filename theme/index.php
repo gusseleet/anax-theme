bb00c26e1cf3f24e47a6c651fe4e3426b3ad2e71
gusseleet <gustavelmgren@gmail.com>
2016-04-18 19:50:01 +0100

    First update

---
+++ theme/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html class='no-js' lang='sv'>
+<head>
+    <meta charset='utf-8'/>
+    <title>Regioner | gel a web template</title>
+    <link rel='stylesheet' type='text/css' href='anax-grid/style.php'/>
+    <link rel='stylesheet' type='text/css' href='anax-grid/regions.css'/>
+</head>
+
+<body>
+<div id='wrapper'>
+
+    <div id='header'>
+        <span class='sitetitle'>GEL THEME</span>
+    </div>
+
+    <div id='navbar'>navbar</div>
+
+    <div id='flash'>flash</div>
+
+    <div id='wrap-featured'>
+        <div id='featured-1'>featured-1</div>
+        <div id='featured-2'>featured-2</div>
+        <div id='featured-3'>featured-3</div>
+    </div>
+
+    <div id='wrap-main'>
+        <div id='main'>main</div>
+        <div id='sidebar'>sidebar</div>
+    </div>
+
+
+
+
+    <div id='wrap-triptych'>
+        <div id='triptych-1'>triptych-1</div>
+        <div id='triptych-2'>triptych-2</div>
+        <div id='triptych-3'>triptych-3</div>
+    </div>
+
+    <div id='wrap-footer-col'>
+        <div id='footer-col-1'>footer-col-1</div>
+        <div id='footer-col-2'>footer-col-2</div>
+        <div id='footer-col-3'>footer-col-3</div>
+        <div id='footer-col-4'>footer-col-4</div>
+    </div>
+
+    <div id='footer'> Footer </div>
+
+</div>
+
+
+
+</body>
+</html>
